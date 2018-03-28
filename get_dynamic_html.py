d09ce81ab8b79f5350cd2726994f65e4b4833f7c
xudi <xudi1989@ruc.edu.cn>
2018-03-28 09:01:23 +0800

    pdf cannot download

--- get_dynamic_html.py
+++ get_dynamic_html.py
@@ -2,7 +2,7 @@
 from selenium import webdriver
 
 fireFoxOptions = webdriver.FirefoxOptions()
-fireFoxOptions.set_headless()
+# fireFoxOptions.set_headless()
 
 def get_profile():
     profile = webdriver.FirefoxProfile()
@@ -20,26 +20,41 @@ profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/ms
 brower_msword = webdriver.Firefox(firefox_profile= profile,firefox_options=fireFoxOptions)
 
 profile = get_profile()
-profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/octet-stream')
-brower_pdf = webdriver.Firefox(firefox_profile= profile,firefox_options=fireFoxOptions)
+profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'text/msword')
+brower_msword2 = webdriver.Firefox(firefox_profile= profile,firefox_options=fireFoxOptions)
 
 profile = get_profile()
 profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/octet-stream')
-brower_excel = webdriver.Firefox(firefox_profile= profile,firefox_options=fireFoxOptions)
+brower_stream = webdriver.Firefox(firefox_profile= profile,firefox_options=fireFoxOptions)
+
+profile = get_profile()
+profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/x-xls')
+brower_xls = webdriver.Firefox(firefox_profile= profile,firefox_options=fireFoxOptions)
+
+profile = get_profile()
+profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/pdf')
+profile.set_preference("plugin.disable_full_page_plugin_for_types", "application/pdf")
+profile.set_preference("pdfjs.disabled", True)
+brower_pdf = webdriver.Firefox(firefox_profile= profile,firefox_options=fireFoxOptions)
+
+brower_dict = {}
+brower_dict['text/html'] = brower_default
+brower_dict['text/msword'] = brower_msword2
+brower_dict['application/msword'] = brower_msword
+brower_dict['application/octet-stream'] = brower_stream
+brower_dict['application/x-xls'] = brower_xls
+brower_dict['application/pdf'] = brower_pdf
+
 
 def get_js_html(url):
     r = brower_default.get(url)
     return brower_default.page_source.encode('utf-8')
 
-def download_js(url,href_text):
-    if href_text.endswith('doc') or href_text.endswith('docx'):
-        brower = brower_msword
-    elif href_text.endswith('xls') or href_text.endswith('xlsx'):
-        brower = brower_excel
-    else:
-        brower = brower_pdf
-    r = brower.get(url)    
+def download_js(url,href_text,content_type):
+    brower = brower_dict[content_type]
+    brower.get(url)    
     button = brower.find_element_by_link_text(href_text)
+    print button.get_attribute("href")
     button.click()
         
 if __name__ == '__main__':
--- get_punishment_details.py
+++ get_punishment_details.py
@@ -2,15 +2,15 @@
 
 import os
 import shutil
-import pandas as pd
-from sqlalchemy import distinct
+import subprocess
 
 from get_dynamic_html import get_js_html,download_js
 from bs4 import BeautifulSoup
 from urlparse import urljoin
-import requests
 import re
 from db_api import Punishment,func
+
+import urllib2
 
 HEADERS = {
     'X-Requested-With': 'XMLHttpRequest',
@@ -25,6 +25,7 @@ get_city = lambda x: x.split('-')[0]
 is_doc_url = lambda x: x.endswith('.doc') or x.endswith('.docx') or x.endswith('.xls') \
                     or x.endswith('.xlsx') or x.endswith('.pdf')
 
+content_pattern = re.compile(r'[Cc]ontent-[Tt]ype:\s*([a-zA-Z]+/[a-zA-Z\-]+)')
 delete_span = re.compile(ur'<span[^>]*>(.*?)</span>')
 #html = delete_span.sub(lambda x: x.group(1),html )
 public_table_kw = re.compile(ur'违法行为\s*(类型|内容){0,1}')
@@ -32,17 +33,28 @@ link_kw = re.compile(ur'(行政){0,1}处罚(信息){0,1}(公示表|公示|表){0
 href_kw = re.compile(ur'.(xls|xlsx|doc|docx|pdf)$')
 img_kw  = re.compile(ur'.(tif|jpg|jpeg|bmp)$')
 
+def get_content_type(url):
+    ret = subprocess.Popen(['curl','-I',url], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    buf = ret.stdout.read().strip()
+    matched = content_pattern.search(buf,re.I)
+    if matched:
+        return matched.group(1)
+    else:
+        return 'none'
+    
 def strip_tags(soup,invalid_tags):
     for tag in invalid_tags: 
         for match in soup.findAll(tag):
             match.replaceWithChildren()
     return soup
 
-def dump_doc(page_url,des_path,text,fname,index):
+def dump_doc(page_url,des_path,text,fname,index,real_url,update_date):
     suffix = fname.split('.')[-1]
-    outfile_name = '.'.join((str(index),suffix))
+    outfile_name = '.'.join(('_'.join((str(index),str(update_date))),suffix))
     outfile_path = os.path.join(des_path,outfile_name)
-    download_js(page_url,text)
+    content_type = get_content_type(real_url)
+    print text,page_url,real_url,content_type
+    download_js(page_url,text,content_type)
     shutil.move(os.path.join(download_path,fname), outfile_path)
     
 def create_punishment_fiels(dbapi,city,des_path):
@@ -52,41 +64,45 @@ def create_punishment_fiels(dbapi,city,des_path):
     session.close()
     print city,count
     cnt = 0
+    city_root_url = 'http://{}.pbc.gov.cn'.format(city)
     for record in cursor:
         print cnt
         cnt += 1
         #case 1: is excel or doc, like tianjing
         if is_doc_url(record.punishment_item_url):
             real_url = '/'.join(record.punishment_item_url.split('//')[-1].split('/')[1:])
-            print real_url
+            print record.punishment_item_url
             this_soup = BeautifulSoup(record.publication_url,'lxml')
             this_tag = this_soup.find('a',attrs = {'href',real_url})
             text = this_tag.get_text()
             fname = real_url.split('/')[-1]
-            dump_doc(record.publication_url,des_path,text,fname,record.index)
+            dump_doc(record.publication_url,des_path,text,fname,record.index,record.punishment_item_url,record.update_date)
             continue
         public_html = get_js_html(record.punishment_item_url)
         soup = BeautifulSoup(public_html,'lxml')
         tag = soup.find(['p','span','td'],text = public_table_kw)
         link_tag  = soup.find('a',text = link_kw,attrs={'href':href_kw})
-        img_tag   = soup.find('a',text = img_kw,attrs={'href':href_kw})
+        img_tag   = soup.find('a',attrs={'href':img_kw})
         #case 2: is table , like beijing
         if tag:
+            print 'table = ',record.punishment_item_url
             table_tag = tag.find_parent('table')
             soup = strip_tags(soup, ['span','p'])
-            outfile_name = '.'.join((str(record.index),'html'))
+            outfile_name = '.'.join(('_'.join((str(record.index),str(record.update_date))),'html'))
             outfile_path = os.path.join(des_path,outfile_name)
-            with open(outfile_path, 'ab') as fout:
-                fout.write(table_tag.prettify().encode('utf-8'))
+            if not os.path.exists(outfile_path):
+                with open(outfile_path, 'ab') as fout:
+                    fout.write(table_tag.prettify().encode('utf-8'))
             continue
         #case 3: is link, like shanghai
         elif link_tag:       
             link_url = link_tag['href']
-            print link_url
+            real_link = urljoin(city_root_url,link_url)
+            print 'link = ',real_link
             text = link_tag.get_text()
             fname = link_url.split('/')[-1]
 #             print record.punishment_item_url,des_path,text,fname,record.update_date,record.punishment_item_url
-            dump_doc(record.punishment_item_url,des_path,text,fname,record.index)
+            dump_doc(record.punishment_item_url,des_path,text,fname,record.index,real_link,record.update_date)
             continue
         elif img_tag:
             print 'img, skipped'
@@ -449,8 +465,8 @@ def test_html():
 '''
     #     html = delete_span.sub(lambda x: x.group(1),html )
     soup = BeautifulSoup(html,'lxml')
-#     soup = strip_tags(soup, ['span','p'])
-#     print soup.prettify()
+    soup = strip_tags(soup, ['span','p'])
+    print soup.prettify()
     tag = soup.find('span',text = public_table_kw)
     link_tag  = soup.find('a',text = link_kw,attrs={'href':href_kw})
 #     print tag
@@ -471,8 +487,16 @@ def get_punish_table():
         if not os.path.exists(des_path):
             os.makedirs(des_path)
         create_punishment_fiels(dbapi,city,des_path)
- 
+
+def test_download():
+    url = r'http://shanghai.pbc.gov.cn/fzhshanghai/113577/114832/114918/3008150/index.html'
+    download_js(url, u'（第13期）行政处罚信息公示表.pdf', 'application/octet-stream')
+
+
 if __name__ == '__main__':
 #     test_html()
-    get_punish_table()
-            
+#     get_punish_table()
+#     test_content_type()
+    test_download()
+
+    
