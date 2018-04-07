cced18535b8b69be443ea21f02de4e14088833bd
xudi <xudi1989@ruc.edu.cn>
2018-04-07 20:51:01 +0800

    add all

--- text_analysis.py
+++ text_analysis.py
@@ -6,6 +6,10 @@ from bs4 import BeautifulSoup
 import re
 
 public_table_kw = re.compile(ur'违法行为\s*(类型|内容){0,1}')
+empty = re.compile('[ \n\t]')
+
+is_replaceble = lambda x: isinstance(x,str) or isinstance(x,unicode)
+
 def is_table_td(tag):
     return tag.name == 'td' and public_table_kw.search(tag.text)
 
@@ -20,21 +24,11 @@ def parse_html(infile):
     htmltxt = htmlpath2txt(infile)
     soup = BeautifulSoup(htmltxt,'lxml')
     tag = soup.find_all(is_table_td)[-1]
-    tag.find_parent('table')
-    this_table = False
-    while True:
-        tag = tag.parent
-        if tag.name != 'table':
-            if hasattr(tag, 'parent'):
-                tag = tag.parent
-            else:
-                break
-        else:
-            this_table = True
-            break
-    if this_table:
-        print tag.prettify()
- 
+    tag = tag.find_parent('table')
+    df = pd.read_html(tag.prettify())[0]
+    df = df.applymap(lambda x:re.sub(empty,'',x) if is_replaceble(x) else x)
+    print df
+    
 if __name__ == '__main__':
     infile = r'/home/xudi/tmp/punishment_source/beijing/0_20180320.html'
     parse_html(infile)
